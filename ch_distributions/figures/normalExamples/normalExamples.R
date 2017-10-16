c9d41a8fe473d27add675e67fcfc62d9337de2ec
Xiang Zhu <xiangzhu.nku@gmail.com>
2017-10-15 22:29:10 -0700

    suppress lib msg and rmv pdf output and rmv axis title

--- ch_distributions/figures/normalExamples/normalExamples.R
+++ ch_distributions/figures/normalExamples/normalExamples.R
@@ -1,10 +1,11 @@
-library(openintro)
+suppressPackageStartupMessages(library(openintro))
 data(COL)
 
-source("normalExamples-data.R")
-source("normalExamples-helpers.R")
+source("~/GitHub/openintro-statistics/ch_distributions/figures/normalExamples/normalExamples-data.R")
+source("~/GitHub/openintro-statistics/ch_distributions/figures/normalExamples/normalExamples-helpers.R")
 
-myPDF("normalExamples.pdf", 7.3, 4.4,
+#myPDF("normalExamples.pdf", 7.3, 4.4,
+par(
       mfrow = c(2, 3),
       mgp = c(2, 0.7, 0),
       mar = c(3, 0, 1, 1))
@@ -17,4 +18,4 @@ QQNorm(obs1, M1, SD1, COL[1])
 QQNorm(obs2, M2, SD2, COL[2])
 QQNorm(obs3, M3, SD3, "#B09A00")
 
-dev.off()
+#dev.off()
