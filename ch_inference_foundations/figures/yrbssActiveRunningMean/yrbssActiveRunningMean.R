9c0c0d50bb2977631418ab4c40cb7dc2d5085609
Xiang Zhu <xiangzhu.nku@gmail.com>
2017-10-22 16:23:57 -0700

    suppress lib msg and rmv pdf output and rmv axis title

--- ch_inference_foundations/figures/yrbssActiveRunningMean/yrbssActiveRunningMean.R
+++ ch_inference_foundations/figures/yrbssActiveRunningMean/yrbssActiveRunningMean.R
@@ -1,18 +1,19 @@
-library(openintro)
+suppressPackageStartupMessages(library(openintro))
 data(COL)
 
 d <- yrbss.samp$physically_active_7d
 xBars <- cumsum(d) / (1:100)
 m <- mean(yrbss$physically_active_7d, na.rm = TRUE)
 
-myPDF('yrbssActiveRunningMean.pdf', 5.5, 2.7,
-      mar=c(3.3, 4.9, 1, 2))
+#myPDF('yrbssActiveRunningMean.pdf', 5.5, 2.7,
+
+par(mar=c(3.3, 4.9, 1, 2))
 
 plot(xBars,
      type = "l",
      axes = FALSE,
      xlab = "",
-     ylab = "Running mean of days\nphysically active per week",
+     ylab = "Running mean",
      col = COL[1],
      lwd = 2,
      ylim = range(xBars))
@@ -20,7 +21,7 @@ axis(1, at = seq(0, 100, 25))
 axis(2, at = seq(0, 5, 1))
 abline(h = m, lty = 2, col = COL[6,3])
 mtext("Sample size", 1, 2)
-dev.off()
+#dev.off()
 
-m
-sd(yrbss$physically_active_7d, na.rm = TRUE)
+#m
+#sd(yrbss$physically_active_7d, na.rm = TRUE)
