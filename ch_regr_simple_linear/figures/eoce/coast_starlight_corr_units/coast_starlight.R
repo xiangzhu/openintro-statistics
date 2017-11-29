07031b399f99604662c53cb08098331ede4029f2
Xiang Zhu <xiangzhu.nku@gmail.com>
2017-11-29 12:54:08 -0800

    rmv pdf print

--- ch_regr_simple_linear/figures/eoce/coast_starlight_corr_units/coast_starlight.R
+++ ch_regr_simple_linear/figures/eoce/coast_starlight_corr_units/coast_starlight.R
@@ -1,13 +1,13 @@
 # load packages -----------------------------------------------------
-library(openintro)
+suppressPackageStartupMessages(library(openintro))
 
 # load data ---------------------------------------------------------
 
-coast_starlight <- read.table("coast_starlight.txt", h = T, sep = "\t")
+coast_starlight <- read.table("~/GitHub/openintro-statistics/ch_regr_simple_linear/figures/eoce/coast_starlight_corr_units/coast_starlight.txt", h = T, sep = "\t")
 
 # plot trave time vs. distance --------------------------------------
 
-pdf("coast_starlight.pdf", 5.5, 4.3)
+#pdf("coast_starlight.pdf", 5.5, 4.3)
 
 par(mar = c(3.8, 3.8, 0.5, 0.5), las = 1, mgp = c(2.7, 0.7, 0), 
     cex.lab = 1.25, cex.axis = 1.25)
@@ -19,4 +19,4 @@ axis(1, at = seq(0, 400, 100))
 axis(2, at = seq(0, 360, 60))
 box()
 
-dev.off()
+#dev.off()
