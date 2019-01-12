3145bbd604ccda46feab0eb09dfc97ddd69f48e4
Xiang Zhu <xiangzhu.nku@gmail.com>
2019-01-11 18:19:23 -0800

    add a version of 6895997 plot for sample mean

---
+++ ch_distributions/figures/6895997/6895997_sam.R
@@ -0,0 +1,63 @@
+suppressPackageStartupMessages(library(openintro))
+data(COL)
+
+#myPDF("6895997.pdf", 5, 2.5,
+par(mar = c(2, 0, 0, 0))
+X <- seq(-4, 4, 0.01)
+Y <- dnorm(X)
+plot(X, Y,
+     type = 'n',
+     axes = FALSE,
+     xlim = c(-3.2, 3.2),
+     ylim = c(0, 0.4),
+     xlab = '', ylab= '')
+abline(h = 0, col = COL[6])
+at <- -3:3
+labels <- expression(mu[F] - 3 * sigma[F]/sqrt(n),
+                     mu[F] - 2 * sigma[F]/sqrt(n),
+                     mu[F] - sigma[F]/sqrt(n),
+                     mu[F],
+                     mu[F] + sigma[F]/sqrt(n),
+                     mu[F] + 2 * sigma[F]/sqrt(n),
+                     mu[F] + 3 * sigma[F]/sqrt(n))
+axis(1, at, labels)
+for (i in 3:1) {
+  these <- (i - 1 <= X & X <= i)
+  polygon(c(i - 1, X[these], i),
+          c(0, Y[these], 0),
+          col = COL[i],
+          border = COL[i])
+  these <- (-i <= X & X <= -i + 1)
+  polygon(c(-i, X[these], -i + 1),
+          c(0, Y[these], 0),
+          col = COL[i],
+          border = COL[i])
+}
+
+# _____ Label 99.7 _____ #
+arrows(-3, 0.03,
+       3, 0.03,
+       code = 3,
+       col = '#444444',
+       length = 0.15)
+text(0, 0.02, '99.7%', pos = 3)
+
+# _____ Label 95 _____ #
+arrows(-2, 0.13,
+       2, 0.13,
+       code = 3,
+       col = '#444444',
+       length = 0.15)
+text(0, 0.12, '95%', pos = 3)
+
+# _____ Label 68 _____ #
+arrows(-1, 0.23,
+       1, 0.23,
+       code = 3,
+       col = '#444444',
+       length = 0.15)
+text(0, 0.22, '68%', pos = 3)
+
+lines(X, Y, col = '#888888')
+abline(h = 0, col = '#AAAAAA')
+#dev.off()
